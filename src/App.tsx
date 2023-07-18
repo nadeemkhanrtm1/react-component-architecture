dbf4d48ac6aaf8f0ffa61cdb47e2b29015bfa064
nadeem-preplaced <102961425+nadeem-preplaced@users.noreply.github.com>
2023-07-18 23:10:30 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+
+const App: React.FC = (props) => {
+  /* -------------------------------------------------------------------------- */
+  /*                                    Props                                   */
+  /* -------------------------------------------------------------------------- */
+  const { name } = props;
+  /* -------------------------------------------------------------------------- */
+  /*           Global Stores Accessing getting values and dispatchers           */
+  /* -------------------------------------------------------------------------- */
+  const nameSelector = useSelector((state) => state.store);
+  /* -------------------------------------------------------------------------- */
+  /*                         local states of components                         */
+  /* -------------------------------------------------------------------------- */
+  const [displayName, setDisplayName] = useState(name);
+  /* -------------------------------------------------------------------------- */
+  /*                                 useEffects                                 */
+  /* -------------------------------------------------------------------------- */
+  useEffect(() => {
+    setDisplayName(nameSelector);
+  },[nameSelector])
+  /* -------------------------------------------------------------------------- */
+  /*                       handlers like Click, Change etc                      */
+  /* -------------------------------------------------------------------------- */
+  const handleClick = (e: MouseEvent) => {
+    setDisplayName(e.target.value);
+  }
+
+  return (
+  <>
+  <input type="text" value={displayName}/>
+  <button>Submit</button>
+  </>
+  )
+}
+
+export default App;
